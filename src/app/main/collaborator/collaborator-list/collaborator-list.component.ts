f59b12f13e0b0443cc19854f2950fd557f530323
porapot <porapot2@gmail.com>
2019-08-23 18:34:46 +0700

    pull

--- src/app/main/collaborator/collaborator-list/collaborator-list.component.ts
+++ src/app/main/collaborator/collaborator-list/collaborator-list.component.ts
@@ -1,4 +1,101 @@
 import { Component, OnInit } from '@angular/core';
+import { SchoolService } from 'app/main/school/school.service';
+import { ActivatedRoute } from '@angular/router';
+import { CollaboratorService } from '../collaborator.service';
+import { FormBuilder } from '@angular/forms';
+
+export interface Students {
+
+  email: string;
+  schoolid: string;
+  
+}
+const contact: Students[] = [
+  {
+    'email': 'abbott@withinpixels.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'porapot@gmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'stp@gmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  },
+  {
+    'email': 'smx@hotmail.com',
+    'schoolid': '5d5b7c7f066dda001a8c28b4'
+  }
+];
 
 @Component({
   selector: 'collaborator-list',
@@ -6,12 +103,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collaborator-list.component.scss']
 })
 export class CollaboratorListComponent implements OnInit {
-  dataSource: null;
-  displayedColumns = ['avatar', 'email', 'phone', 'jobTitle', 'buttons'];
 
-  constructor() { }
-
-  ngOnInit() {
+  collaborators = [];
+  collaborator = {
+    email: ""
   }
 
+
+  dataSource: any;
+  displayedColumns = [ 'email','schoolid','buttons'];
+
+
+  constructor(private collab: CollaboratorService) { }
+
+  ngOnInit() {
+    this.dataSource = contact;
+    
+  }
+
+  // getList() {
+  //   this.collab.getlist();
+  //   this.collab.gettinglist.subscribe((res: any) => {
+  //     if (res.status == 200) {
+  //       this.collaborators = res.data;
+  //     }
+  //   })
+  // }
+
+  // onDelete(_id) {
+  //   //console.log(_id);
+  //   this.collab.delete(_id);
+  //   this.collab.deleted.subscribe((res: any) => {
+  //     if (res.status == 200) {
+  //       this.getList();
+  //     }
+  //   })
+  // } 
 }
+
